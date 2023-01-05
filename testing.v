0b9038ec06a11662509abae85eab57136dfc7e69
Quardin Lyttle <lyttleqd@gmail.com>
2023-01-04 20:56:11 -0500

    Added asynchronous reset

--- testing.v
+++ testing.v
@@ -1,5 +1,5 @@
-module newBinary(clk2,clk4,clk8,clk16,clk);
-  input clk;
+module newBinary(clk2,clk4,clk8,clk16,clk,reset);
+  input clk,reset;
   output clk2,clk4,clk8,clk16;
   reg [3:0] counter;
   assign clk2=counter[0];
@@ -8,9 +8,15 @@ module newBinary(clk2,clk4,clk8,clk16,clk);
   assign clk16=counter[3];
   
   
-  always@(posedge clk)
-      counter<=counter+1;
-  
+  always@(posedge clk or posedge reset)
+    begin
+      if (reset)
+        counter<=0;
+      else
+
+    counter<=counter+1;
+    end
+ 
   
 endmodule
 

046c7bf90145dfe04d99dc963165449bbfcf8fd0
Quardin Lyttle <lyttleqd@gmail.com>
2023-01-04 20:52:44 -0500

    Added a Binary Counter clock output module to testing file

--- testing.v
+++ testing.v
@@ -1,1 +1,16 @@
+module newBinary(clk2,clk4,clk8,clk16,clk);
+  input clk;
+  output clk2,clk4,clk8,clk16;
+  reg [3:0] counter;
+  assign clk2=counter[0];
+  assign clk4=counter[1];
+  assign clk8=counter[2];
+  assign clk16=counter[3];
+  
+  
+  always@(posedge clk)
+      counter<=counter+1;
+  
+  
+endmodule
 
